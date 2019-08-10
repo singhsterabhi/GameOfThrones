241080717f00123729d5b515a5b7b648abedbc82
singhsterabhi <singhsterabhi@gmail.com>
2019-08-10 20:31:41 +0530

    image loading loader

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./Home.module.scss";
 import map from "./map.webp";
 
-const home = () => {
-  return (
-    <div className={classes.Home}>
-      <h1 className={classes.Heading}>Game of Thrones World Map</h1>
-      <img src={map} alt="Game of Thrones World Map" />
-      <p>
-        <a
-          href="https://awoiaf.westeros.org/index.php/World_Map_(HBO_Game_Of_Thrones)"
-          target="_blank"
-          rel="noopener noreferrer">
-          Read More
-        </a>
-      </p>
-    </div>
-  );
-};
+import Spinner from "../Spinner/Spinner";
 
-export default home;
+class Home extends Component {
+  state = {
+    imageLoading: true
+  };
+  render() {
+    return (
+      <div className={classes.Home}>
+        <h1 className={classes.Heading}>Game of Thrones World Map</h1>
+        <img
+          src={map}
+          alt="Game of Thrones World Map"
+          onLoad={() => this.setState({ imageLoading: false })}
+        />
+        {this.state.imageLoading ? (
+          <Spinner />
+        ) : (
+          <p>
+            <a
+              href="https://awoiaf.westeros.org/index.php/World_Map_(HBO_Game_Of_Thrones)"
+              target="_blank"
+              rel="noopener noreferrer">
+              Read More
+            </a>
+          </p>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Home;
